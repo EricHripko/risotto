0106d3b66c4f7d748396e30fa7f6cab5b84401fe
Michele Cipriano <sc15mc@leeds.ac.uk>
2016-02-15 17:46:31 +0000

    Basic structure of RestaurantHandler setted up.

--- server/src/comp2541/bison/restaurant/RestaurantHandler.java
+++ server/src/comp2541/bison/restaurant/RestaurantHandler.java
@@ -1,5 +1,33 @@
 package comp2541.bison.restaurant;
 
-public class RestaurantHandler {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+/**
+ * 
+ * @author michelecipriano
+ *
+ */
+public class RestaurantHandler extends AbstractHandler {
+
+	public RestaurantHandler(String dbString) {
+		// TODO Connect to database
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
