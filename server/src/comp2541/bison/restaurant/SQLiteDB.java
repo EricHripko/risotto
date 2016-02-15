21b53ee52cefe529f5eb2936db07f7c722a49032
Ilyass Taouil <sc14it@leeds.ac.uk>
2016-02-15 21:13:12 +0000

    SQLiteDB's basic database connection/creation implemented in setUp() method.

--- server/src/comp2541/bison/restaurant/SQLiteDB.java
+++ server/src/comp2541/bison/restaurant/SQLiteDB.java
@@ -1,5 +1,78 @@
 package comp2541.bison.restaurant;
 
+import java.sql.*;
+
 public class SQLiteDB extends Database {
 
+	/*
+	 * String dbName
+	 */
+	private String dbName = null;
+
+	/*
+	 * Connection object
+	 */
+	private Connection conn = null;
+
+	/*
+	 * Statement object
+	 */
+	private Statement stmt = null;
+
+	//public constructor
+	public SQLiteDB(String dbName) {
+		super(dbName);
+
+		//fields initialization
+		this.dbName = dbName;
+	}
+
+	/*
+	 * setUp() method
+	 * 
+	 * connect to existing database, otherwise it creates one with the specified name.
+	 */
+	public void setUp() {
+
+		try {
+			//Connect to database
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+
+			//Create new table in the database
+			stmt = conn.createStatement();
+			String query = "CREATE TABLE IF NOT EXISTS Booking " +
+						   "(customerName TEXT     NOT NULL,"    +
+					       " phoneNumber  TEXT     NOT NULL, "   + 
+					       " email        TEXT     NOT NULL,"    +
+					       " partySize    INT      NOT NULL,"    +  
+					       " date         TEXT     NOT NULL,"    +
+					       " time         TEXT     NOT NULL,"    +
+					       " primary key(customerName,"          +
+					       				"phoneNumber, "	         +
+					       				"date,"                  +
+					       				"time)"					 +
+					       	");";
+			//Execute query
+			stmt.executeUpdate(query);
+			
+			//Free resources
+			stmt.close();
+			conn.close();
+		} 
+		catch(Exception e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+		
+		//Console message
+		System.out.println("Set up completed/updated");
+	}
+
+	@Override
+	void insertBooking(String customerName, String phoneNumber, String email, int partySize, String date, String time) {
+		// TODO Auto-generated method stub
+		
+	}
 }
+
