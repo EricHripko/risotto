486e48d7d57c8b247cf6d6931dbf584e18b6396d
Michele Cipriano <sc15mc@leeds.ac.uk>
2016-02-15 17:10:30 +0000

    Basic RestaurantServer structure.
    The class extends Jetty's Server class, the constructor takes two
    parameters, a port and a threadpool.

--- server/src/comp2541/bison/restaurant/RestaurantServer.java
+++ server/src/comp2541/bison/restaurant/RestaurantServer.java
@@ -1,5 +1,28 @@
 package comp2541.bison.restaurant;
 
-public class RestaurantServer {
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.thread.ThreadPool;
 
+/**
+ * 
+ * @author michelecipriano
+ *
+ */
+public class RestaurantServer extends Server {
+
+	/**
+	 * 
+	 * @param port
+	 * @param pool
+	 */
+	public RestaurantServer(int port, ThreadPool pool) {
+		// Uses a ThreadPool allow multiple threads.
+		super(pool);
+		
+		// Add a ServerConnector with specified port to the server.
+		ServerConnector serverConnector = new ServerConnector(this);
+		serverConnector.setPort(port);
+		this.addConnector(serverConnector);
+	}
 }
