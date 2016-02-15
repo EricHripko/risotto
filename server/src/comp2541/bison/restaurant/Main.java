8d36a5df8691f66d6fa145465ff29e8ecca8f1df
Michele Cipriano <sc15mc@leeds.ac.uk>
2016-02-15 17:49:30 +0000

    Main method setted up, server launched.

--- server/src/comp2541/bison/restaurant/Main.java
+++ server/src/comp2541/bison/restaurant/Main.java
@@ -1,7 +1,24 @@
 package comp2541.bison.restaurant;
 
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
+
+/**
+ * 
+ * @author michelecipriano
+ *
+ */
 public class Main {
+	
 	public void main(String[] args) {
-		// TODO
+		// TODO Test the code
+		try {
+			RestaurantServer restaurantServer = new RestaurantServer(8080, new ExecutorThreadPool());
+			restaurantServer.setHandler(new RestaurantHandler("nameofdatabase.db"));
+			restaurantServer.start();
+			restaurantServer.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
