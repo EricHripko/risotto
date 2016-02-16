bfc6fc14f90003132119a2cd57d6b45e2750a9a5
Eric Hripko <yksirius@gmail.com>
2016-02-16 11:35:51 +0000

    Booking service
    A service that makes calls to the server to add and manage the bookings.
    Add ignore file to avoid checking in IDE files

---
+++ client/scripts/booking.js
@@ -0,0 +1,47 @@
+/**
+ * Create a new booking object.
+ * @param customerName Name of the person booking.
+ * @param phoneNumber Phone number of the person booking.
+ * @param emailAddress Email address of the person booking.
+ * @param date Booking date.
+ * @param partySize Party size.
+ * @constructor
+ */
+var Booking = function (customerName, phoneNumber, emailAddress, partySize, date) {
+    /**
+     * Booking date.
+     */
+    this.date = date;
+    if(this.date instanceof Date)
+        this.date = this.date.getTime();
+    /**
+     * How many people booked.
+     */
+    this.partySize = partySize;
+    /**
+     * Name of the person booking.
+     */
+    this.customerName = customerName;
+    /**
+     * Phone Number of the person booking.
+     */
+    this.phoneNumber = phoneNumber;
+    /**
+     * Email Address of the person booking.
+     */
+    this.emailAddress = emailAddress;
+};
+
+/**
+ * Service for managing bookings.
+ */
+var BookingService = {
+    /**
+     * Make a new booking
+     * @param booking Booking instance.
+     * @returns Promise that represents the request.
+     */
+    make: function (booking) {
+        return server.bookings.create(booking);
+    }
+};
