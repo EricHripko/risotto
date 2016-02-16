f9af62f04068f9086e529929844a4726bd3a1cf7
sc15sc <sc15sc@leeds.ac.uk>
2016-02-16 18:40:13 +0000

    first commit

---
+++ client/scripts/mainPage.js
@@ -0,0 +1,11 @@
+//keep track of the cell which have been clicked by 
+//recording row and column of table 
+
+function myfunction(x) {
+    alert("clicked cell at: " + x.cellIndex +"," + x.parentNode.rowIndex); 
+}
+
+
+for(i =0; i<5; i++){
+    "hello" + "<br>";  
+}
