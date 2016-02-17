d3248045d32e8f9d3b77f60de7694556e769f01a
Michele Cipriano <sc15mc@leeds.ac.uk>
2016-02-17 17:27:29 +0000

    Handle of "/bookings" request.
    The request of "/bookings" are now handled and stored in a JSONObject,
    the next step is to store it in the database.

--- server/src/comp2541/bison/restaurant/RestaurantHandler.java
+++ server/src/comp2541/bison/restaurant/RestaurantHandler.java
@@ -1,9 +1,7 @@
 package comp2541.bison.restaurant;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.json.JSONObject;
 
 /**
  * 
@@ -19,6 +18,10 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
  */
 public class RestaurantHandler extends AbstractHandler {
 
+	/**
+	 * 
+	 * @param dbString
+	 */
 	public RestaurantHandler(String dbString) {
 		// TODO Connect to database
 	}
@@ -29,6 +32,7 @@ public class RestaurantHandler extends AbstractHandler {
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
+		
 		// Below is necessary to pass CORS check
 		String origin = request.getHeader("Origin");
 		response.addHeader("Access-Control-Allow-Origin", origin);
@@ -36,27 +40,48 @@ public class RestaurantHandler extends AbstractHandler {
 		response.addHeader("Access-Control-Allow-Headers", "Accept, Content-type");
 		response.addHeader("Access-Control-Max-Age", "1728000");
 		
-		if(request.getMethod().equalsIgnoreCase("options")) {
-			response.setStatus(200);
-			try (PrintWriter out = response.getWriter()) {
-				out.write("");
+		// TODO Handle requests and db (Issue #6)
+		
+		// TODO Add proper logging to the server (Issue #10)
+		//System.out.printf("%s %s", request.getMethod(), request.getRequestURI());
+		//System.out.println("Body:");
+		
+		if (request.getMethod().equalsIgnoreCase("GET")) {
+			System.out.println("Requested URI: " + request.getRequestURI());
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().println("");
+		} else if (request.getMethod().equalsIgnoreCase("POST")) {
+			if (request.getRequestURI() == "/bookings") {
+				BufferedReader requestBodyBR = request.getReader(); // Reader for the body of the HTTP message
+				StringBuilder sb = new StringBuilder();				// Auxiliary object to tranform body to JSON
+				String line;										// String used to read from BufferedReader
+				
+				// Reading the body:
+				while ((line = requestBodyBR.readLine()) != null) {
+					sb.append(line);
+				}
+				
+				// StringBuilder to JSONObject:
+				JSONObject jsonBody = new JSONObject(sb.toString());
+				
+				// TODO Take elements from JSON object and put them to db.
+			} else {
+				// TODO Handle other requests.
 			}
-			return;
+		} else {
+			// Dump the Request
+			
+			// TODO Why do you need to read a line?
+			//try (BufferedReader in = request.getReader()) {
+			//	System.out.println(in.readLine());
+			//}
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().println("");
 		}
 		
-		//TODO Handler code
-		
-		// Dump the request
-		System.out.printf("%s %s", request.getMethod(), request.getRequestURI());
-		System.out.println("Body:");
-		try(BufferedReader in = request.getReader()) {
-			System.out.println(in.readLine());
-		}
-		// Send dummy response
-		response.setStatus(200);
-		try(PrintWriter out = response.getWriter()) {
-			out.write("");
-		}
+		// The request has been handled correctly.
+		baseRequest.setHandled(true);
 	}
 
 }
