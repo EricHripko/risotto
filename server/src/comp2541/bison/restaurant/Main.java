b850a1cf9641b01117560158b97970126041a18f
Jones Mabea <jonemabz@gmail.com>
2016-02-18 16:07:58 +0000

    Added Request Log for server

--- server/src/comp2541/bison/restaurant/Main.java
+++ server/src/comp2541/bison/restaurant/Main.java
@@ -1,5 +1,9 @@
 package comp2541.bison.restaurant;
 
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.NCSARequestLog;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.RequestLogHandler;
 import org.eclipse.jetty.util.thread.ExecutorThreadPool;
 
 /**
@@ -14,6 +18,22 @@ public class Main {
 		try {
 			RestaurantServer restaurantServer = new RestaurantServer(8080, new ExecutorThreadPool());
 			restaurantServer.setHandler(new RestaurantHandler("restaurantdatabase.db"));
+			
+			HandlerCollection handlers = new HandlerCollection();
+			//Handler to log requests
+			RequestLogHandler requestLogHandler = new RequestLogHandler();
+			RestaurantHandler restaurantHandler = new RestaurantHandler("nameofdatabase.db");
+			handlers.setHandlers(new Handler[]{requestLogHandler, restaurantHandler });
+			restaurantServer.setHandler(handlers);
+			
+			//Creates log file for requests to the server
+			NCSARequestLog requestLog = new NCSARequestLog("./restaurantserver.log");
+			requestLog.setRetainDays(90);
+			requestLog.setAppend(true);
+			requestLog.setExtended(false);
+			requestLog.setLogTimeZone("GMT");
+			requestLogHandler.setRequestLog(requestLog);
+			
 			restaurantServer.start();
 			restaurantServer.join();
 		} catch (Exception e) {
