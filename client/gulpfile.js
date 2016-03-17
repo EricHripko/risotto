42247ca995a407c78d0aeecac19e6678c4f374f6
sc15sc <sc15sc@leeds.ac.uk>
2016-03-17 12:41:22 +0000

    payment form is completed

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,8 +1,27 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require('browser-sync').create(); 
+var reload = browserSync.reload; 
 
-gulp.task("styles", function() {
-    return gulp.src("sass/*.sass")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("css"));
-});
+gulp.task("styles", function(){
+    gulp.src('sass/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/client/css'))
+    .pipe(browserSync.reload({stream: true})); 
+}); 
+
+gulp.task('serve', function(){
+       browserSync.init({
+       server: {
+        baseDire: './'
+       }
+       }); 
+
+gulp.watch('sass/*.sass', ['styles']); 
+gulp.watch('*.html').on('change', browserSync.reload);  
+gulp.watch('css/*.css').on('change', browserSync.reload); 
+gulp.watch('scripts/*.js').on('change', browserSync.reload); 
+    
+}); 
+
+gulp.task('default', ['styles', 'serve']); 
