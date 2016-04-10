064ec41bd6368acbe2a59bbcb4e353fb288063b2
EricHripko <yksirius@gmail.com>
2016-04-10 15:29:24 +0100

    Discard the changes to Gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,27 +1,8 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var browserSync = require('browser-sync').create(); 
-var reload = browserSync.reload; 
 
-gulp.task("styles", function(){
-    gulp.src('sass/*.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('app/client/css'))
-    .pipe(browserSync.reload({stream: true})); 
-}); 
-
-gulp.task('serve', function(){
-       browserSync.init({
-       server: {
-        baseDire: './'
-       }
-       }); 
-
-gulp.watch('sass/*.sass', ['styles']); 
-gulp.watch('*.html').on('change', browserSync.reload);  
-gulp.watch('css/*.css').on('change', browserSync.reload); 
-gulp.watch('scripts/*.js').on('change', browserSync.reload); 
-    
-}); 
-
-gulp.task('default', ['styles', 'serve']); 
+gulp.task("styles", function() {
+    return gulp.src("sass/*.sass")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("css"));
+});
