c65a06e03464b0fbe66fbf31c10902d01810c5e8
Dailand10 <itaouil95@gmail.com>
2016-04-30 18:46:19 +0100

    Changed script data mock-up.

--- server/importScriptDB.sh
+++ server/importScriptDB.sh
@@ -2,3 +2,4 @@
 # Drop tables from the database
 sqlite3 restaurantdatabase.db ".import tables.txt RestaurantTable"
 sqlite3 restaurantdatabase.db ".import meals.txt Meal"
+sqlite3 restaurantdatabase.db ".import bookings.txt Booking"
