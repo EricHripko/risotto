b983a19145bc484854ab8081ad7982cbf8e8e851
EricHripko <yksirius@gmail.com>
2016-05-03 23:13:46 +0100

    Add JavaDoc for the notification classes

--- server/src/comp2541/bison/restaurant/EmailBookingReminder.java
+++ server/src/comp2541/bison/restaurant/EmailBookingReminder.java
@@ -10,36 +10,43 @@ import org.apache.log4j.Logger;
 import com.sendgrid.*;
 import com.sendgrid.SendGrid.Email;
 
-
 /**
+ * Class to handle Email notifications.
  * @author Jones Agwata
- *
- */
-/*
- * Class to handle Email notifications
  */
 public class EmailBookingReminder {
-	private Booking pbooking;
-	public SendGrid sendgrid  = new SendGrid("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");
-	
+	/**
+	 * Logger for this class.
+	 */
 	static Logger log = Logger.getLogger(EmailBookingReminder.class.getName());
 	
+	/**
+	 * Booking that reminder will be sent for.
+	 */
+	private Booking pbooking;
 	
+	/**
+	 * SendGrid API library.
+	 */
+	public SendGrid sendgrid  = new SendGrid("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");
+	
+	/**
+	 * Create an email reminder for the booking.
+	 * @param booking Booking.
+	 */
 	public EmailBookingReminder(Booking booking){
 		pbooking=booking;
-		//Send Email to notify of successful Booking
+		// Send Email to notify of successful Booking
 		sendEmail();
-		//set three hour allowance and set reminder
-			
+		
+		// Set three hour allowance and set reminder
 		int scheduleAllowance = Math.toIntExact(pbooking.getUnixStart()-(3600*3));
 		scheduleReminder(scheduleAllowance);
-		
-		
 	}
-	/*
-	 * sends email if email is provided
-	 */
 	
+	/**
+	 * Send an email to the customer if email was provided.
+	 */
 	public void sendEmail(){
 		long unixSeconds = pbooking.getUnixStart();
 		Date date = new Date(unixSeconds*1000L);
@@ -60,10 +67,11 @@ public class EmailBookingReminder {
 			log.fatal(e.getMessage());
 		}
 	}
-	/*
-	 * sends email reminder 3 hours before booking
-	 */
 	
+	/**
+	 * Schedule an email reminder to be sent on the given time.
+	 * @param time Unix time of sent.
+	 */
 	public void scheduleReminder(int time){
 		
 		long unixSeconds = pbooking.getUnixStart();
