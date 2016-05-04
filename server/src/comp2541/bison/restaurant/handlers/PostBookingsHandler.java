2a94deaa169960ac93cec0d6c023a814e62640ee
EricHripko <yksirius@gmail.com>
2016-05-04 17:18:55 +0100

    Ensure that preferred table is tried first when making a booking

--- server/src/comp2541/bison/restaurant/handlers/PostBookingsHandler.java
+++ server/src/comp2541/bison/restaurant/handlers/PostBookingsHandler.java
@@ -27,36 +27,53 @@ public class PostBookingsHandler extends BaseHandler {
 			// Get all the available tables and check if the request can be satisfied
 			ArrayList<Table> availableTables = database.getAvailableTables(booking.getUnixStart(), booking.getUnixEnd());
 			boolean tableFound = false;
-
+			
+			// Identify whether it is possible to put a booking in the preferred table
+			Table prefferedTable = null;
 			for (Table table : availableTables) {
-				if (table.getSize() >= booking.getPartySize()) {
-					// If the table is found then the request could be satisfied:
-					tableFound = true;
-
-					// Update table ID
-					booking.setTable(table);
-
-					int referenceNumber = database.insertBooking(booking);
-					booking.setReferenceNumber(referenceNumber);
-					
-					if(booking.getPhoneNumber() != null ){
-						new SMSBookingReminder(booking);
-					}
-					if(booking.getEmail() != null){
-						new EmailBookingReminder(booking);
-					}
-					
-
-					// Send OK and referenceNumber to the client.
-					response.setStatus(HttpServletResponse.SC_OK);
-					response.getWriter().println(booking.getJSONObject().toString());
-
-					break;
-				}
+				if(table.getId() == booking.getTable().getId())
+					prefferedTable = table;
 			}
+			// Ensure booking can fit on the preferred table
+			if(prefferedTable != null && prefferedTable.getSize() < booking.getPartySize())
+				prefferedTable = null;
+			
+			// Place the booking on the table
+			if(prefferedTable != null) {
+				booking.setTable(prefferedTable);
+				tableFound = true;
+			}
+			else
+				for (Table table : availableTables) {
+					if (table.getSize() >= booking.getPartySize()) {
+						// If the table is found then the request can be satisfied
+						tableFound = true;
+	
+						// Update table ID
+						booking.setTable(table);
+	
+						break;
+					}
+				}
 
+			if(tableFound) {
+				int referenceNumber = database.insertBooking(booking);
+				booking.setReferenceNumber(referenceNumber);
+				
+				if(booking.getPhoneNumber() != null ){
+					new SMSBookingReminder(booking);
+				}
+				if(booking.getEmail() != null){
+					new EmailBookingReminder(booking);
+				}
+				
+
+				// Send OK and referenceNumber to the client.
+				response.setStatus(HttpServletResponse.SC_OK);
+				response.getWriter().println(booking.getJSONObject().toString());
+			}
 			// If after the search the table hasn't been found then the request couldn't be satisfied:
-			if (tableFound == false) {
+			else {
 				// Send JSON error message to the client
 				JSONObject jsonError = new JSONObject();
 				jsonError.put("errorMessage", "There are no tables available at the requested time.");
